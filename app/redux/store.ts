c8574ce22954a12864641e023161342fe3454da3
Manuel Rueda <manuel.rueda.un@gmail.com>
2016-06-14 22:23:24 -0300

    add redux-devtools

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -1,6 +1,9 @@
-import { combineReducers, createStore, Store } from 'redux'
+import { combineReducers, createStore, Store, compose } from 'redux'
+const isDevelopment = require('electron-is-dev');
 import { dojoReducer, IDojo } from './dojoReducer'
 import { kataReducer, IKata } from './kataReducer'
+
+const devTools = <Function>require('remote-redux-devtools');
 
 const totalReducer = combineReducers<IDojoStore>({
     dojo: dojoReducer,
@@ -11,5 +14,13 @@ export interface IDojoStore {
     dojo: IDojo,
     katas: Array<IKata>
 }
+let enhancer:any;
+if (isDevelopment){
+    enhancer = <any>compose(devTools({
+        name: 'Electro Dojo', realtime: true,
+        hostname: 'localhost', port: 6868,
+        maxAge: 30
+    }));
+}
 
-export const store = createStore<IDojoStore>(totalReducer);
+export const store = createStore<IDojoStore>(totalReducer, enhancer);
